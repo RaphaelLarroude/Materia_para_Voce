d2e8f34abd725657275cc9b96671ae1b9a7f5a11
Raphael Larroudé <rapha@raphaelcosta.com.br>
2025-11-11 23:12:52 -0300

    feat(courses): Add 'Matemática' course data
    Includes course details, structure, and a link to study materials for the 'Matemática' course.

--- utils/course.ts
+++ utils/course.ts
@@ -44,7 +44,49 @@ const createDefaultCourses = (): Course[] => {
             }
         ]
     };
-    return [inglesCourse];
+
+    const matematicaCourse: Course = {
+        id: generateId(),
+        title: 'Matemática',
+        teacher: 'Raphael Costa',
+        teacherId: 'rapha-admin-id',
+        icon: 'BeakerIcon',
+        imageUrl: 'https://images.pexels.com/photos/374918/pexels-photo-374918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        status: 'Não iniciado',
+        progress: 0,
+        classrooms: [],
+        years: [],
+        content: [
+            {
+                id: generateId(),
+                title: '2025',
+                illustrationUrl: 'https://images.pexels.com/photos/207692/pexels-photo-207692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                classrooms: [],
+                years: [],
+                categories: [
+                    {
+                        id: generateId(),
+                        title: 'Roteiro de Estudos',
+                        illustrationUrl: 'https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                        classrooms: [],
+                        years: [],
+                        materials: [
+                            {
+                                id: generateId(),
+                                title: 'Estudo para PT - Mat - 3 tri - 2025',
+                                type: 'link',
+                                content: "https://1drv.ms/b/c/99af5b4d952510d4/Eas_a2CFnhFMtFRiPlx5-tIB7G4AQ4LnHbUGb24xGTLe9Q?e=TbFaKq",
+                                classrooms: [],
+                                years: [],
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
+
+    return [inglesCourse, matematicaCourse];
 };
 
 
